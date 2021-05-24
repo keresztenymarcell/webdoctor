f6a8ade1ff6c0e7c9abc7ee3ae917a703cdb42b9
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-05-24 15:35:24 +0200

    Correct route to /list in server.py

--- server.py
+++ server.py
@@ -7,15 +7,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
-
 @app.route("/list")
 def list_page():
     questions = data_handler.sort_questions(data_handler.DATA_FILE_PATH_questions, True)
-    return render_template(list.html, data_handler.QUESTIONS_HEADER, questions)
+    return render_template(list.html, header=data_handler.QUESTIONS_HEADER, questions=questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug=True,
+    )
