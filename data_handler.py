228131cda65624aa50f6ca204446a5a4c63e153f
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-05-24 21:53:27 +0200

    Add sorting option to /list

--- data_handler.py
+++ data_handler.py
@@ -4,9 +4,10 @@ QUESTIONS_HEADER = ['Id', 'Submission Time', 'View Number', 'Vote Number', 'Titl
 ANSWERS_HEADER = ['Id', 'Submission Time', 'Vote Number', 'Question Id', 'Message', 'Image']
 
 
-def sort_questions(sorting_direction=False, sorting_key='submission_time'):
+def sort_questions(order_by, order_direction):
+    sorting = True if order_direction == 'desc' else False
     read_csvfile = connection.open_csvfile(connection.DATA_FILE_PATH_QUESTIONS)
-    sorted_listofdict = sorted(read_csvfile, key=lambda x: x[sorting_key], reverse=sorting_direction)
+    sorted_listofdict = sorted(read_csvfile, key=lambda x: x[order_by], reverse=sorting)
     return sorted_listofdict
 
 # writing back to csv according to id(ascending)? + use QUESTION_KEYS/ANSWER_KEYS to make order within rows
--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request, redirect
 
-import data_handler
+import data_handler, connection
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/list")
-@app.route("/list")
-def list_page():
-    questions = data_handler.sort_questions(True)
-    return render_template('list.html', header=data_handler.QUESTIONS_HEADER, questions=questions)
+def hello():
+    return 'Hello World'
+
+
+@app.route("/list", defaults={'sort_parameters': 'order_by=submission_time&order_direction=desc'})
+@app.route("/list/<sort_parameters>")
+def list_page(sort_parameters):
+    order_by = sort_parameters.split("&")[0].split("=")[1]
+    order_direction = sort_parameters.split("&")[1].split("=")[1]
+    questions = data_handler.sort_questions(order_by, order_direction)
+    return render_template('list.html', header=data_handler.QUESTIONS_HEADER, keys=connection.QUESTION_KEYS, questions=questions)
 
 
 if __name__ == "__main__":
