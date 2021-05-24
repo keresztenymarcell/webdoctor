3ecaed84d7ecf6064ecde01f7423d7bf214f2d06
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-05-24 22:24:39 +0200

    Correct data file path

--- connection.py
+++ connection.py
@@ -1,6 +1,6 @@
 import csv
 
-DATA_FILE_PATH_QUESTIONS = '/home/gyongyi/projects/ask-mate-1-python-gyongyi0888/sample_data/question.csv'
+DATA_FILE_PATH_QUESTIONS = 'sample_data/question.csv'
 DATA_FILE_PATH_ANSWERS = 'sample_data/answer.csv'
 
 QUESTION_KEYS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
