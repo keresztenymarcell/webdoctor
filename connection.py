21f75ffbfd187dabfc65ba4ed7abc0ecc0ed39ba
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-05-25 15:50:04 +0200

    Accept changes, delete read_from_csv() from connection.py

--- connection.py
+++ connection.py
@@ -12,7 +12,6 @@ def open_csvfile(filepath):
         read_csvfile = list(csv.DictReader(csvfile))
         return read_csvfile
 
-<<<<<<< HEAD
 
 def write_files(file_path, fieldnames, data_table):
     csvfile = open(file_path, "w")
@@ -20,13 +19,4 @@ def write_files(file_path, fieldnames, data_table):
     dict_writer.writeheader()
     dict_writer.writerows(data_table)
     csvfile.close()
-=======
 
-def read_from_csv(filepath):
-    input_file = csv.DictReader(open(filepath))
-    database = []
-    for row in input_file:
-        database.append(row)
-
-    return database
->>>>>>> 153da2886ab5dd4f810182875e59b8c7b179e7e7
