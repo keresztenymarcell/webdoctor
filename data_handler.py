5f430bfce56c9c60e5b848bd2173aa54b3404d82
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-05-25 16:46:17 +0200

    write_questions() function

--- data_handler.py
+++ data_handler.py
@@ -10,11 +10,14 @@ def sort_questions(order_by, order_direction):
     sorted_listofdict = sorted(read_csvfile, key=lambda x: x[order_by], reverse=sorting)
     return sorted_listofdict
 
+
 def delete_question_by_id(question_id):
     questions = connection.open_csvfile(connection.DATA_FILE_PATH_QUESTIONS)
     for question in questions:
         if question_id == question["id"]:
             questions.remove(question)
+
+    connection.write_files(connection.DATA_FILE_PATH_QUESTIONS, connection.QUESTION_KEYS, questions)
 
     # still need to write back to the file
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 import data_handler, connection
 
@@ -27,10 +27,33 @@ def display_question(question_id):
                 return render_template('display_question.html', question=question, answers=answers)
 
 
+@app.route("/add-question", methods=["GET", "POST"])
+def write_questions():
+    questions = connection.open_csvfile(connection.DATA_FILE_PATH_QUESTIONS)
+    if request.method == "POST":
+
+        if questions is []:
+            question_id = 0
+        else:
+            question_id = int(questions[-1]["id"]) + 1
+
+
+        get_data = request.form.to_dict()
+        get_data["id"] = str(question_id)
+        questions.append(get_data)
+        connection.write_files(connection.DATA_FILE_PATH_QUESTIONS,connection.QUESTION_KEYS,questions)
+        return redirect(url_for("display_question", question_id=question_id))
+
+    return render_template('question.html')
+    
+
+
+
+
 @app.route("/question/<question_id>/delete")
 def delete_question(question_id):
     data_handler.delete_question_by_id(question_id)
-    return redirect("list_page")
+    return redirect(url_for("display_question", question_id=question_id))
 
 
 
