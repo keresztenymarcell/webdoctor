243076772f5e77ccef6db5084d16fd49e8198a05
gyongyi0888 <gyongyi.andrea.kudlik@gmail.com>
2021-05-25 18:20:19 +0200

    Accept incoming changes, add back question.csv

--- server.py
+++ server.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 import data_handler, connection
 
-ANSWER_KEYS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/list", defaults={'sort_parameters': 'order_by=submission_time&order_direction=desc'})
-@app.route("/list/<sort_parameters>")
-def list_page(sort_parameters):
-    order_by = sort_parameters.split("&")[0].split("=")[1]
-    order_direction = sort_parameters.split("&")[1].split("=")[1]
+@app.route("/list")
+def list_page():
+    order_by = request.args.get('order_by', 'submission_time')
+    order_direction = request.args.get('order_direction', 'desc')
     questions = data_handler.sort_questions(order_by, order_direction)
     return render_template('list.html', header=data_handler.QUESTIONS_HEADER, keys=connection.QUESTION_KEYS, questions=questions)
 
@@ -45,7 +44,7 @@ def write_questions():
         return redirect(url_for("display_question", question_id=question_id))
 
     return render_template('question.html')
-    
+
 
 
 
